498df34176bb252628d96dbebcb4cc444c895907
whx2010nj@gmail.com <whx2010nj@gmail.com>
2016-11-17 21:38:43 +0800

    spring first

---
+++ src/main/java/com/whx/nju/springmvc/po/Orders.java
@@ -0,0 +1,99 @@
+package com.whx.nju.springmvc.po;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Created by wanghongxiang on 16/11/13.
+ */
+public class Orders implements Serializable {
+
+    private Integer id;
+
+    private Integer userId;
+
+    private String number;
+
+    private Date createtime;
+
+    private String note;
+
+//    用户信息
+    private User user;
+
+
+//    订单系统
+    private List<Orderdetail> orderdetails;
+
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public Date getCreatetime() {
+        return createtime;
+    }
+
+    public void setCreatetime(Date createtime) {
+        this.createtime = createtime;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public List<Orderdetail> getOrderdetails() {
+        return orderdetails;
+    }
+
+    public void setOrderdetails(List<Orderdetail> orderdetails) {
+        this.orderdetails = orderdetails;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Orders{" +
+                "id=" + id +
+                ", userId=" + userId +
+                ", number='" + number + '\'' +
+                ", createtime=" + createtime +
+                ", note='" + note + '\'' +
+                ", user=" + user +
+                ", orderdetails=" + orderdetails +
+                '}';
+    }
+}
---
+++ src/main/webapp/WEB-INF/jsp/itemsList.jsp
@@ -0,0 +1,51 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: wanghongxiang
+  Date: 16/11/17
+  Time: 下午1:14
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" pageEncoding="UTF-8" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/fmt" prefix="fmt" %>
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+    <title>查询商品列表</title>
+</head>
+<body>
+
+<form action="${pageContext.request.contextPath}/item/queryItem.action" method="post">
+    查询条件:
+    <table width="100%" border="1">
+        <tr>
+            <td>
+                <input type="submit" value="查询"/>
+            </td>
+        </tr>
+    </table>
+    商品列表:
+    <table width="100%" border="1">
+        <tr>
+            <td>商品名称</td>
+            <td>商品价格</td>
+            <td>生成日期</td>
+            <td>商品描述</td>
+            <td>操作</td>
+        </tr>
+
+        <c:forEach items="${itemsList}" var="item">
+            <tr>
+                <td>${item.name}</td>
+                <td>${item.price}</td>
+                    <%--<td><fmt:formatDate value="${item.createtime}" pattern="yyyy-MM-dd HH-mm-ss"/></td>--%>
+                <td>${item.createtime}</td>
+                <td>${item.detail}</td>
+                <td><a href="${pageContext.request.contextPath}/item/editItem.action?id=${item.id}">修改</a></td>
+            </tr>
+        </c:forEach>
+    </table>
+</form>
+
+</body>
+</html>
